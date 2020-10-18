fb610224e405c2246ecaa815cb92d1a3f2553b00
frod2793 <tmdwhd0344@naver.com>
2020-10-18 18:55:09 +0900

    타이틀 레벨 틀

---
+++ Assets/Scenes/Gamemanager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamemanager : MonoBehaviour
+{
+    public void ChangeScene()
+    {
+        Application.LoadLevel("level"); //새로운 레벨을 로드합니다.
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+   
+}
---
+++ Assets/Scenes/MapSich.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MapSich : MonoBehaviour
+{
+    public Button left, right;
+
+    public List<Transform> MapBuffer;
+
+    bool updateUI;
+
+    int currentIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateUI();
+        UpdateMap();
+    }
+
+    void Update()
+    {
+        if (updateUI)
+        {
+            updateUI = false;
+            UpdateUI();
+        }
+    }
+    // Update is called once per frame
+    void UpdateUI()
+    {
+        left.interactable = currentIndex > 0;
+        right.interactable = currentIndex < MapBuffer.Count - 1;
+    }
+
+    void UpdateMap()
+    {
+        for (int i = 0; i < MapBuffer.Count; i++) 
+        {
+          
+                MapBuffer[i].gameObject.SetActive(i == currentIndex);
+            
+        }
+    }
+    public void SwichMap(bool leftORright)
+    {
+        currentIndex += leftORright ? -1 : 1;
+        updateUI = true;
+
+        UpdateMap();
+    }
+}
