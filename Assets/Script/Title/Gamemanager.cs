13100c468686a1d420c66a518586debecbbf5b8f
frod2793 <tmdwhd0344@naver.com>
2020-10-25 23:48:34 +0900

    버튼 리소스 적용

--- Assets/Script/Title/Gamemanager.cs
+++ Assets/Script/Title/Gamemanager.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Gamemanager : MonoBehaviour
 {
     public void ChangeScene()
     {
-        //Application.LoadLevel("level"); //새로운 레벨을 로드합니다.
+        SceneManager.LoadScene("MapLevel"); //새로운 레벨을 로드합니다.
     }
     // Use this for initialization
     void Start()
