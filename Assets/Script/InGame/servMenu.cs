f601b0c1df4402b0e5de649da0e9894bb5dba208
frod2793 <tmdwhd0344@naver.com>
2020-10-30 01:16:19 +0900

    일시정지

---
+++ Assets/Script/InGame/servMenu.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class servMenu : MonoBehaviour
+{
+    private bool IsMenue = false;
+   
+    public GameObject menue;
+    private bool IsPause = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    public void menu()
+    {
+
+       
+            menue.SetActive(true);
+            IsMenue = true;
+        
+
+        
+            
+                Time.timeScale = 0;
+                IsPause = true;
+             
+           
+          
+        
+    }
+    public void menuerun()
+    {
+        
+            menue.SetActive(false);
+            IsMenue = false;
+       
+
+            Time.timeScale = 1;
+            IsPause = false;
+ 
+            return;
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+
+    }
+}
