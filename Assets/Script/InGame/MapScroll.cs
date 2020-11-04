e193d80a51af16bc590e4f2ee15395b6d40c85f8
taewooo0513 <taewooo0513@gmail.com>
2020-11-04 15:22:55 +0900

    배치초반

---
+++ Assets/Script/InGame/MapScroll.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapScroll : MonoBehaviour
+{
+    public GameObject PlayerPos;
+    private MeshRenderer render;
+    private float offset;
+    public float speed;
+    public float CurSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurSpeed = speed;
+        render = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = new Vector3(PlayerPos.transform.position.x+5,1,1);
+        offset += Time.deltaTime * CurSpeed;
+        render.material.mainTextureOffset = new Vector2(offset, 0);
+    }
+}
