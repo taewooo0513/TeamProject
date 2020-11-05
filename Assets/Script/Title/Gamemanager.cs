2e888ca77b7829f5b352283013a68ab3ebab9fd1
frod2793 <tmdwhd0344@naver.com>
2020-11-06 03:34:37 +0900

    스테이지 선택 및 대거 패치

--- Assets/Script/Title/Gamemanager.cs
+++ Assets/Script/Title/Gamemanager.cs
@@ -10,6 +10,11 @@ public class Gamemanager : MonoBehaviour
     {
         SceneManager.LoadScene("MapLevel"); //새로운 레벨을 로드합니다.
     }
+
+    public void Change()
+    {
+        SceneManager.LoadScene("ChoosStage"); //새로운 레벨을 로드합니다.
+    }
     // Use this for initialization
     void Start()
     {
